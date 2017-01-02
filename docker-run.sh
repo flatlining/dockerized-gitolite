4077c38f3015e015c074b132ad6fef1199d43e25
Matias Schertel <mschertel@gmail.com>
2017-01-02 20:59:49 -0200

    add scripts

---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -p 22022:22 -e SSH_KEY="$(cat ~/admin.pub)" -v $HOME/docker-data/git/repositories:/home/git/repositories -v $HOME/docker-data/git/ssh:/etc/ssh --restart=always --name gitolite rpi-gitolite:arm
